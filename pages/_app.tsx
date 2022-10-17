8383bcde461c43e5be38e689b2d484c666c7f7d5
taleed <tall.eed.2022@gmail.com>
2022-10-17 14:07:04 +0100

    consume theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,11 @@ import "@fontsource/changa";
 
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import theme from "theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
--- theme/index.ts
+++ theme/index.ts
@@ -5,14 +5,14 @@ const overrides = {
   config: { initialColorMode: "light", useSystemColorMode: false },
   styles: {
     global: (props: StyleFunctionProps) => ({
-      html: {
-        overflowX: "hidden",
-      },
-      // body: {
-      //   color: props.colorMode === "dark" ? "whiteAlpha.900" : "gray.800",
-      //   bg: props.colorMode === "dark" ? "#0d1119" : "#f6f6f6",
+      // html: {
       //   overflowX: "hidden",
       // },
+      body: {
+        color: props.colorMode === "dark" ? "grey.300" : "grey.900",
+        bg: props.colorMode === "dark" ? "brand.black" : "white",
+        overflowX: "hidden",
+      },
     }),
   },
   direction: "rtl",
@@ -21,7 +21,29 @@ const overrides = {
     body: `"Changa", sans-serif`,
   },
   colors: {
-    //
+    brand: {
+      primary: "#4C31BC",
+      secondary: "#06D3F4",
+      black: "#222",
+    },
+    purple: {
+      100: "#EDEAF8",
+      200: "#B8A8F6",
+      300: "#7C62E5",
+      400: "#6A4DE1",
+      500: "#3A229D",
+    },
+    grey: {
+      100: "#F5F5F6",
+      200: "#E7E8E8",
+      300: "#D6D6D6",
+      400: "#A5A6A6",
+      500: "#828788",
+      600: "#6C6E6F",
+      700: "#2F3133",
+      800: "#414447",
+      900: "#2F3133",
+    },
   },
 };
 
