24475a25c37c6b7265dcdf371bddeddcd8ae3d86
addy <addayounes8@gmail.com>
2022-12-15 17:59:49 +0100

    added personal page to dashboard

--- components/dashboard/mobileNav.tsx
+++ components/dashboard/mobileNav.tsx
@@ -32,8 +32,12 @@ const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
   const [profile, setProfile] = useState({
     first_name: "",
     last_name: "",
+    username: "",
     avatar_url: null,
   });
+
+  console.log("username", profile.username);
+
   const SignOutHandler = async () => {
     const { error } = await supabaseClient.auth.signOut();
     if (error) {
@@ -47,7 +51,7 @@ const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
     const getProfileData = async () => {
       const { data, error } = await supabaseClient
         .from("profiles")
-        .select("first_name, last_name, avatar_url")
+        .select("first_name, last_name, avatar_url, username")
         .eq("id", user?.id)
         .single();
       if (!error) setProfile(data);
@@ -63,26 +67,25 @@ const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
     <Flex
       mr={{ base: 0, md: 60 }}
       px={{ base: 4, md: 4 }}
-      height="20"
-      alignItems="center"
-      bg="white"
-      borderBottomWidth="1px"
-      borderBottomColor="gray.200"
+      height='20'
+      alignItems='center'
+      bg='white'
+      borderBottomWidth='1px'
+      borderBottomColor='gray.200'
       justifyContent={{ base: "space-between", md: "flex-end" }}
-      {...rest}
-    >
+      {...rest}>
       <IconButton
         display={{ base: "flex", md: "none" }}
         onClick={onOpen}
-        variant="outline"
-        aria-label="open menu"
+        variant='outline'
+        aria-label='open menu'
         icon={<FiMenu />}
       />
 
-      <Link href="/" passHref>
-        <HStack spacing={5} cursor="pointer">
-          <Image src="/logo.svg" alt="talleed_logo" />
-          <chakra.span fontWeight={800} fontSize="2xl" color="white">
+      <Link href='/' passHref>
+        <HStack spacing={5} cursor='pointer'>
+          <Image src='/logo.svg' alt='talleed_logo' />
+          <chakra.span fontWeight={800} fontSize='2xl' color='white'>
             تليــد
           </chakra.span>
         </HStack>
@@ -90,32 +93,33 @@ const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
 
       <Flex alignItems={"center"}>
         <Menu>
-          <MenuButton
-            py={2}
-            transition="all 0.3s"
-            _focus={{ boxShadow: "none" }}
-          >
+          <MenuButton py={2} transition='all 0.3s' _focus={{ boxShadow: "none" }}>
             <HStack>
               <Avatar
                 size={"sm"}
                 src={`${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/avatars/${profile.avatar_url}`}
                 name={`${profile.first_name} ${profile.last_name}`}
               />
-              <Text fontSize="sm">{`${profile.first_name} ${profile.last_name}`}</Text>
+              <Text fontSize='sm'>{`${profile.first_name} ${profile.last_name}`}</Text>
               <Box display={{ base: "none", md: "flex" }}>
                 <FiChevronDown />
               </Box>
             </HStack>
           </MenuButton>
-          <MenuList bg="white" borderColor="gray.200">
+          <MenuList bg='white' borderColor='gray.200'>
             <MenuItem>
-              <Link href="/dashboard/editprofile">
-                <chakra.span display="block">اعدادات الحساب</chakra.span>
+              <Link href={`/authors/${profile.username}`}>
+                <chakra.span display='block'>الصفحة الشخصية</chakra.span>
+              </Link>
+            </MenuItem>
+            <MenuItem>
+              <Link href='/dashboard/editprofile'>
+                <chakra.span display='block'>اعدادات الحساب</chakra.span>
               </Link>
             </MenuItem>
             <MenuDivider />
             <MenuItem>
-              <chakra.span onClick={SignOutHandler} display="block">
+              <chakra.span onClick={SignOutHandler} display='block'>
                 تسجيل الخروج
               </chakra.span>
             </MenuItem>
