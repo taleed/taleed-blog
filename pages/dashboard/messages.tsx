0414b5ca1fd14162d7f1e665af83eaeff8452c60
addy <addayounes8@gmail.com>
2022-12-15 15:50:24 +0100

    added pagination to messages

--- pages/dashboard/messages.tsx
+++ pages/dashboard/messages.tsx
@@ -1,51 +1,57 @@
-import Layout from '@/layouts/Dashboard';
-import { getPagination, ITEMS_IN_PAGE } from '@/utils/paginationConfig';
-import { Box, Button, Heading, IconButton, Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, Stack, Table, TableContainer, Tbody, Td, Th, Thead, Tr, useDisclosure } from '@chakra-ui/react';
-import { ReactElement, useEffect, useState } from 'react'
-import { AiOutlineEye } from 'react-icons/ai';
-
+import PaginationBar from "@/components/PaginationBar";
+import Layout from "@/layouts/Dashboard";
+import { getPagination, ITEMS_IN_PAGE } from "@/utils/paginationConfig";
+import {
+  Box,
+  Heading,
+  IconButton,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { ReactElement, useEffect, useState } from "react";
+import { AiOutlineEye } from "react-icons/ai";
 
 const Notifications = () => {
-  const [messages, setMessages] = useState<any[]>([])
-  const [count, setCount] = useState(0)
-  const [message, setMessage] = useState<string>("")
+  const [messages, setMessages] = useState<any[]>([]);
+  const [count, setCount] = useState(0);
+  const [message, setMessage] = useState<string>("");
+  const [currentPage, setCurrentPage] = useState(0);
   const messageModal = useDisclosure();
-  const [currentPage, setCurrentPage] = useState(0)
 
   useEffect(() => {
-    new Promise( async () => {
-      const {from, to} = getPagination(currentPage, ITEMS_IN_PAGE);
+    new Promise(async () => {
+      const { from, to } = getPagination(currentPage, ITEMS_IN_PAGE);
       await fetch(`/api/contact?from=${from}&to=${to}`)
-      .then((res) => res.json())
-      .then(data => {
-        setMessages(data.data)
-        setCount(data.count)
-      })
-    })
-  }, [currentPage])
-
-  const isLastPage = () => {
-    return (ITEMS_IN_PAGE * (currentPage+1) + ITEMS_IN_PAGE) > count
-  }
-
-  const handleNext = () => {
-    setCurrentPage(currentPage+1)
-  }
-
-  const handleBack = () => {
-    setCurrentPage(currentPage-1)
-  }
+        .then((res) => res.json())
+        .then((data) => {
+          setMessages(data.data);
+          setCount(data.count);
+        });
+    });
+  }, [currentPage]);
 
   const openMessageModal = (message: string) => {
-    messageModal.onOpen()
-    setMessage(message)
-  }
+    setMessage(message);
+    messageModal.onOpen();
+  };
 
   return (
     <Box px={8}>
-    <Heading>الرسائل</Heading>
-    <TableContainer bg="white" mt={16}>
-        <Table variant="simple">
+      <Heading>الرسائل ({count})</Heading>
+      <TableContainer bg='white' mt={16}>
+        <Table variant='simple'>
           <Thead>
             <Tr>
               <Th>ID</Th>
@@ -56,16 +62,16 @@ const Notifications = () => {
             </Tr>
           </Thead>
           <Tbody>
-            {messages?.map((d, i) => (
+            {messages?.map((d) => (
               <Tr bg={d.color} key={d.id}>
-                 <Td>{d.id}</Td>
+                <Td>{d.id}</Td>
                 <Td>{d.topic}</Td>
                 <Td>{d.type}</Td>
                 <Td>{d.email}</Td>
                 <Th>
                   <IconButton
                     onClick={() => openMessageModal(d.message)}
-                    aria-label="profile details"
+                    aria-label='profile details'
                     icon={<AiOutlineEye />}
                   />
                 </Th>
@@ -74,27 +80,24 @@ const Notifications = () => {
           </Tbody>
         </Table>
       </TableContainer>
-      <Stack direction="row">
-        {!isLastPage() && <Button onClick={handleNext}>التالي</Button>}
-        {currentPage !== 0 && <Button onClick={handleBack}>السابق</Button>}
-      </Stack>
-      <Modal
-        isOpen={messageModal.isOpen}
-        onClose={messageModal.onClose}
-      >
+      <PaginationBar
+        max={count}
+        itemsPerPage={ITEMS_IN_PAGE}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
+      <Modal isOpen={messageModal.isOpen} onClose={messageModal.onClose}>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>نص الرسالة</ModalHeader>
           <ModalCloseButton />
-          <ModalBody textAlign={"center"}>
-              {message}
-          </ModalBody>
-      </ModalContent>
+          <ModalBody textAlign={"center"}>{message}</ModalBody>
+        </ModalContent>
       </Modal>
     </Box>
-  )
-}
+  );
+};
 
-Notifications .getLayout = (page: ReactElement) => <Layout>{page}</Layout>;
+Notifications.getLayout = (page: ReactElement) => <Layout>{page}</Layout>;
 
-export default Notifications
+export default Notifications;
