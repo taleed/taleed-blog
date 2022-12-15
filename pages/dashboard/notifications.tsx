17bcaa6e0ac12cdbc14893a6ceafc874f4e74802
addy <addayounes8@gmail.com>
2022-12-15 15:47:31 +0100

    added loading screen to notifications

--- pages/dashboard/notifications.tsx
+++ pages/dashboard/notifications.tsx
@@ -1,3 +1,4 @@
+import Loading from "@/components/loading";
 import PaginationBar from "@/components/PaginationBar";
 import Layout from "@/layouts/Dashboard";
 import { getPagination, ITEMS_IN_PAGE } from "@/utils/paginationConfig";
@@ -8,9 +9,12 @@ const Notifications = () => {
   const [notification, setNotifications] = useState<any[]>([]);
   const [currentPage, setCurrentPage] = useState(0);
   const [count, setCount] = useState(0);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     new Promise(async () => {
+      setLoading(true);
+
       const { from, to } = getPagination(currentPage, ITEMS_IN_PAGE);
       await fetch(`/api/notifications/get-notifications?from=${from}&to=${to}`)
         .then((res) => res.json())
@@ -18,40 +22,46 @@ const Notifications = () => {
           setNotifications(data.data);
           setCount(data.count);
         });
+
+      setLoading(false);
     });
   }, [currentPage]);
 
   return (
     <Box px={8}>
       <Heading>اشعارات ({count})</Heading>
-      <TableContainer bg='white' mt={16}>
-        <Table variant='simple'>
-          <Thead>
-            <Tr>
-              <Th>ID</Th>
-              <Th>الاشعار</Th>
-              <Th>التوقيت</Th>
-              <Th>التاريخ</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            {notification?.map((d, i) => (
-              <Tr key={d.id}>
-                <Td>{d.id}</Td>
-                <Td position='relative' textColor={d.color !== "white" && d.color}>
-                  <span
-                    style={{ backgroundColor: d.color !== "white" ? d.color : "black" }}
-                    className='notification__types-badge'
-                  />
-                  {d.text}
-                </Td>
-                <Td>{new Date(d.created_at).toLocaleTimeString()}</Td>
-                <Td>{new Date(d.created_at).toLocaleDateString()}</Td>
+      {loading ? (
+        <Loading />
+      ) : (
+        <TableContainer bg='white' mt={16}>
+          <Table variant='simple'>
+            <Thead>
+              <Tr>
+                <Th>ID</Th>
+                <Th>الاشعار</Th>
+                <Th>التوقيت</Th>
+                <Th>التاريخ</Th>
               </Tr>
-            ))}
-          </Tbody>
-        </Table>
-      </TableContainer>
+            </Thead>
+            <Tbody>
+              {notification?.map((d, i) => (
+                <Tr key={d.id}>
+                  <Td>{d.id}</Td>
+                  <Td position='relative' textColor={d.color !== "white" && d.color}>
+                    <span
+                      style={{ backgroundColor: d.color !== "white" ? d.color : "black" }}
+                      className='notification__types-badge'
+                    />
+                    {d.text}
+                  </Td>
+                  <Td>{new Date(d.created_at).toLocaleTimeString()}</Td>
+                  <Td>{new Date(d.created_at).toLocaleDateString()}</Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        </TableContainer>
+      )}
       <PaginationBar
         max={count}
         itemsPerPage={ITEMS_IN_PAGE}
