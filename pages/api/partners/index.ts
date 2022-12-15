16de87f506ed4bbef42004ee04eaf40bedd66695
addy <addayounes8@gmail.com>
2022-12-15 16:50:19 +0100

    refactored partners

--- pages/api/partners/index.ts
+++ pages/api/partners/index.ts
@@ -2,59 +2,68 @@ import { supabaseAdmin } from "@/utils/supabaseAdmin";
 import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { NextApiHandler } from "next";
 
-const handler:NextApiHandler = async (req, res) => {
+const handler: NextApiHandler = async (req, res) => {
   //list
   //add
   //edit
 
   const supabase = createServerSupabaseClient({ req, res });
 
-  const { data:user } = await supabase.auth.getUser()
-  const { data:profile } = await supabaseAdmin.from('profiles').select('id, is_admin, username').eq("id", user.user?.id)
+  const { data: user } = await supabase.auth.getUser();
+  const { data: profile } = await supabaseAdmin
+    .from("profiles")
+    .select("id, is_admin, username")
+    .eq("id", user.user?.id);
 
-  if (!profile?.[0].is_admin && (req.method === "POST" || req.method === "PUT" || req.method === "PATCH") ) {
-    res.status(401).json({ message: "هذا المستخدم غير مسموح له باجراء هذه العملية" })
+  if (
+    !profile?.[0].is_admin &&
+    (req.method === "POST" || req.method === "PUT" || req.method === "PATCH")
+  ) {
+    res.status(401).json({ message: "هذا المستخدم غير مسموح له باجراء هذه العملية" });
   }
 
   if (req.method === "GET") {
-    const {error, data} = await supabaseAdmin.from('partners').select("*")
+    const { error, data } = await supabaseAdmin.from("partners").select("*");
 
     if (error) {
-      return res.status(500).json(error)
+      return res.status(500).json(error);
     }
 
-    return res.status(200).json(data)
+    return res.status(200).json(data);
   }
 
-  if (req.method ===  "POST") {
-    const { name, description, image } = JSON.parse(req.body)
-    const {error} = await supabaseAdmin.from('partners').insert({
+  if (req.method === "POST") {
+    const { name, description, image } = JSON.parse(req.body);
+    const { error, data: partner } = await supabaseAdmin
+      .from("partners")
+      .insert({
+        name: name,
+        description: description,
+        image: image,
+      })
+      .select("*");
+
+    if (error) {
+      return res.status(500).json(error);
+    }
+
+    return res.status(200).json({ message: "تم اضافة شريك بنجاح", partner });
+  }
+
+  if (req.method === "PUT") {
+    const { name, description, image } = JSON.parse(req.body);
+    const { error } = await supabaseAdmin.from("partners").update({
       name: name,
-      description:description,
-      image: image
-    })
+      description: description,
+      image: image,
+    });
 
     if (error) {
-      return res.status(500).json(error)
+      return res.status(500).json(error);
     }
 
-    return res.status(200).json("تم اضافة شريك بنجاح")
+    return res.status(200).json("تم التعديل بنجاح");
   }
+};
 
-  if (req.method ===  "PUT") {
-    const { name, description, image } = JSON.parse(req.body)
-    const {error} = await supabaseAdmin.from('partners').update({
-      name: name,
-      description:description,
-      image: image
-    })
-
-    if (error) {
-      return res.status(500).json(error)
-    }
-
-    return res.status(200).json("تم التعديل بنجاح")
-  }
-}
-
-export default handler
+export default handler;
--- pages/dashboard/partners.tsx
+++ pages/dashboard/partners.tsx
@@ -1,8 +1,37 @@
+import Loading from "@/components/loading";
 import Layout from "@/layouts/Dashboard";
 import { supabase } from "@/utils/supabaseClient";
-import { Image, Box, FormControl, FormLabel, Heading, IconButton, Input, Modal, ModalBody, ModalCloseButton,
-        ModalContent, ModalHeader, ModalOverlay, Table, TableContainer, Tbody, Td, Th, Thead, Tr, useDisclosure,
-       useToast, Spinner, Textarea, Button, Stack, FormErrorMessage } from "@chakra-ui/react"
+import {
+  Image,
+  Box,
+  FormControl,
+  FormLabel,
+  Heading,
+  IconButton,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+  useDisclosure,
+  useToast,
+  Spinner,
+  Textarea,
+  Button,
+  Stack,
+  FormErrorMessage,
+  Flex,
+  chakra,
+} from "@chakra-ui/react";
 import { useUser } from "@supabase/auth-helpers-react";
 import { ReactElement, useEffect, useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
@@ -15,18 +44,17 @@ type FormValues = {
 };
 
 const ManagePartners = () => {
-
-  const [data, setData] = useState<any[]>([])
+  const [data, setData] = useState<any[]>([]);
   const [uploading, setUploading] = useState<boolean>(false);
   const [imgUrl, setImgUrl] = useState<string | undefined>(undefined);
+  const [loading, setLoading] = useState(false);
   const modal = useDisclosure();
   const toast = useToast();
-  const user = useUser()
+  const user = useUser();
 
   function makeid(length: number) {
     var result = "";
-    var characters =
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     var charactersLength = characters.length;
     for (var i = 0; i < length; i++) {
       result += characters.charAt(Math.floor(Math.random() * charactersLength));
@@ -44,9 +72,7 @@ const ManagePartners = () => {
     const fileExt = file.name.split(".").pop();
     const filePath = `${user?.id}_${makeid(20)}.${fileExt}`;
 
-    const { error: uploadError } = await supabase.storage
-      .from("avatars")
-      .upload(filePath, file);
+    const { error: uploadError } = await supabase.storage.from("avatars").upload(filePath, file);
 
     if (uploadError) {
       setUploading(false);
@@ -59,43 +85,46 @@ const ManagePartners = () => {
   };
 
   const fetchData = async () => {
-    await fetch('/api/partners')
-    .then((data) => data.json())
-    .then((data) => {
-      setData(data)
-    })
-  }
+    await fetch("/api/partners")
+      .then((data) => data.json())
+      .then((data) => {
+        setData(data);
+      });
+  };
 
   useEffect(() => {
     new Promise(async () => {
-      await fetchData()
-    })
-  }, [setData])
+      setLoading(true);
+      await fetchData();
+      setLoading(false);
+    });
+  }, [setData]);
 
   const showModal = () => {
-    modal.onOpen()
-  }
+    modal.onOpen();
+  };
 
   const handleDelete = async (id: number) => {
-    await fetch('/api/partners/'+id, {
-     method: "DELETE"
+    await fetch("/api/partners/" + id, {
+      method: "DELETE",
     })
-    .then((data) => data.json())
-    .then(() => {
-      toast({
-        title: "تم حذف المقال بنجاح",
-        status: "error",
-        duration: 5000,
-        isClosable: true,
-        position: "top-right",
-        onCloseComplete: async () => {
-          await fetchData()
-        }
+      .then((data) => data.json())
+      .then(() => {
+        toast({
+          title: "تم حذف المقال بنجاح",
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+          position: "top-right",
+          onCloseComplete: async () => {
+            await fetchData();
+          },
+        });
+      })
+      .catch((error: any) => {
+        console.log(error);
       });
-    }).catch((error:any) => {
-      console.log(error)
-    })
-  }
+  };
 
   const {
     setValue,
@@ -104,183 +133,183 @@ const ManagePartners = () => {
     handleSubmit,
     formState: { errors, isSubmitting },
   } = useForm<FormValues>({ mode: "all" });
-  const onSubmit: SubmitHandler<FormValues> = async ({
-    description,
-    name,
-    image,
-  }) => {
+  const onSubmit: SubmitHandler<FormValues> = async ({ description, name, image }) => {
+    try {
+      let data = await fetch("/api/partners", {
+        method: "POST",
+        body: JSON.stringify({
+          name,
+          description,
+          image,
+        }),
+      }).then((res) => res.json());
 
-      try {
-        let data = await fetch('/api/partners', {
-          method: "POST",
-          body: JSON.stringify({
-            name,
-            description,
-            image
-          })
-        }).then(res => res.json())
-
-        if (data.length > 0) {
-          toast({
-            title: "تمت العملية بنجاح",
-            status: "success",
-            duration: 5000,
-            isClosable: true,
-            position: "top-right",
-            onCloseComplete: async () => {
-              await  fetchData()
-            }
-          });
-        }
-      } catch (error) {
-        console.log(error)
+      if (data?.message.lenth > 0) {
+        setData((prev) => [data.partner, ...prev]);
+        toast({
+          title: "تمت العملية بنجاح",
+          status: "success",
+          duration: 3000,
+          isClosable: true,
+          position: "top-right",
+          onCloseComplete: () => modal.onClose(),
+        });
       }
+    } catch (error: any) {
+      console.log(error);
+      toast({
+        title: "حدث خطأ!",
+        description: error.message,
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+        position: "top-right",
+      });
+    }
   };
-
 
   return (
     <Box px={8}>
-      <Heading>شركاؤنا</Heading>
-      <IconButton
+      <Heading>شركاؤنا ({data.length})</Heading>
+      <Button
+        marginBlock={8}
         onClick={() => showModal()}
-        aria-label="profile details"
-        icon={<AiFillPlusCircle />}
-      />
-      <TableContainer mt={6} bg="white" >
-        <Table variant="simple">
-          <Thead>
-            <Tr>
-              <Th>اسم الشريك</Th>
-              <Th>نبذة حوله</Th>
-              <Th>العمليات</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            {data?.map((d:any, i:number) => (
-              <Tr key={d.id}>
-                <Td>{d.name}</Td>
-                <Td>{d.description}</Td>
-                <Td>
-                  <IconButton
-                    onClick={() => handleDelete(d.id)}
-                    aria-label="profile details"
-                    icon={<AiFillDelete />}
-                  />
-                </Td>
+        leftIcon={<AiFillPlusCircle />}
+        aria-label='profile details'>
+        أضف شريك
+      </Button>
+      {loading ? (
+        <Loading />
+      ) : data.length ? (
+        <TableContainer mt={6} bg='white'>
+          <Table variant='simple'>
+            <Thead>
+              <Tr>
+                <Th>اسم الشريك</Th>
+                <Th>نبذة حوله</Th>
+                <Th>العمليات</Th>
               </Tr>
-            ))}
-          </Tbody>
-        </Table>
-      </TableContainer>
-      <Modal
-        isOpen={modal.isOpen}
-        onClose={modal.onClose}
-      >
+            </Thead>
+            <Tbody>
+              {data?.map((d: any, i: number) => (
+                <Tr key={d.id}>
+                  <Td>{d.name}</Td>
+                  <Td>{d.description}</Td>
+                  <Td>
+                    <IconButton
+                      onClick={() => handleDelete(d.id)}
+                      aria-label='profile details'
+                      icon={<AiFillDelete />}
+                    />
+                  </Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        </TableContainer>
+      ) : (
+        <Flex paddingBlock={16} width='100%' justifyContent='center'>
+          <chakra.p>لا يوجد شركاء</chakra.p>
+        </Flex>
+      )}
+      <Modal isOpen={modal.isOpen} onClose={modal.onClose}>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader> {"اضف مقالا"}</ModalHeader>
           <ModalCloseButton />
           <ModalBody textAlign={"center"}>
-          <form onSubmit={handleSubmit(onSubmit)}>
-          <Box display="inline-block" w="full">
-            <Image
-              mb={2}
-              rounded="lg"
-              alt="blog image"
-              borderRadius="lg"
-              w="full"
-              src={`${
-                process.env.NEXT_PUBLIC_SUPABASE_URL
-              }/storage/v1/object/public/avatars/${
-                watch!("image") === undefined
-                  ? "default.png"
-                  : watch!("image")
-              }`}
-            ></Image>
-            <FormControl mb={4}>
-              <FormLabel
-                htmlFor="img"
-                w="full"
-                bg="grey.200"
-                rounded="md"
-                textAlign="center"
-                py={2}
-                px={4}
-                display="flex"
-                alignItems="center"
-                justifyContent="center"
-                _hover={{ bg: "grey.300", cursor: "pointer" }}
-                _focus={{ outline: "none" }}
-              >
-                {uploading ? <Spinner size="md" /> : "قم باختيار صورة "}
-              </FormLabel>
-              <Input
-                visibility="hidden"
-                position="absolute"
-                type="file"
-                id="img"
-                accept="image/*"
-                onChange={(e) => uploadImage(e)}
-                disabled={uploading}
-              />
-            </FormControl>
-          </Box>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <Box display='inline-block' w='full'>
+                <Image
+                  mb={2}
+                  rounded='lg'
+                  alt='blog image'
+                  borderRadius='lg'
+                  w='full'
+                  src={`${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/avatars/${
+                    watch!("image") === undefined ? "default.png" : watch!("image")
+                  }`}></Image>
+                <FormControl mb={4}>
+                  <FormLabel
+                    htmlFor='img'
+                    w='full'
+                    bg='grey.200'
+                    rounded='md'
+                    textAlign='center'
+                    py={2}
+                    px={4}
+                    display='flex'
+                    alignItems='center'
+                    justifyContent='center'
+                    _hover={{ bg: "grey.300", cursor: "pointer" }}
+                    _focus={{ outline: "none" }}>
+                    {uploading ? <Spinner size='md' /> : "قم باختيار صورة "}
+                  </FormLabel>
+                  <Input
+                    visibility='hidden'
+                    position='absolute'
+                    type='file'
+                    id='img'
+                    accept='image/*'
+                    onChange={(e) => uploadImage(e)}
+                    disabled={uploading}
+                  />
+                </FormControl>
+              </Box>
 
-            <FormControl mb={4}>
-              <FormLabel>اسم الشريك</FormLabel>
-              <Input
-                required={true}
-                {...register!("name", {
-                  required: "هذا الحقل مطلوب",
-                })}></Input>
-              <FormErrorMessage>{errors.name?.message}</FormErrorMessage>
-            </FormControl>
-            <FormControl  mb={2}>
-              <FormLabel>وصف موجز حول الشريك</FormLabel>
-              <Textarea
-                maxLength={2000}
-                required={true}
-                {...register!("description", {
-                  required: "هذا الحقل مطلوب",
-                })}></Textarea >
-              <FormErrorMessage>{errors.description?.message}</FormErrorMessage>
-            </FormControl>
-            <Stack mb={2} alignItems={"flex-start"}>
-              <Button
-                w="fit-content"
-                size="lg"
-                p={6}
-                bg={"brand.secondary"}
-                color={"white"}
-                _hover={{
-                  opacity: 0.8,
-                }}
-                _focus={{ opacity: 0.9, color: "white", outline: "none" }}
-                _focusWithin={{
-                  opacity: 0.9,
-                  bg: "brand.secondary",
-                }}
-                _disabled={{
-                  bg: "#81EAFB6B",
-                  pointerEvents: "none",
-                }}
-                borderRadius={10}
-                variant="solid"
-                isLoading={isSubmitting}
-                type="submit"
-              >
-                تاكيد
-              </Button>
-            </Stack>
-
-          </form>
+              <FormControl mb={4}>
+                <FormLabel>اسم الشريك</FormLabel>
+                <Input
+                  required={true}
+                  {...register!("name", {
+                    required: "هذا الحقل مطلوب",
+                  })}></Input>
+                <FormErrorMessage>{errors.name?.message}</FormErrorMessage>
+              </FormControl>
+              <FormControl mb={2}>
+                <FormLabel>وصف موجز حول الشريك</FormLabel>
+                <Textarea
+                  maxLength={2000}
+                  required={true}
+                  {...register!("description", {
+                    required: "هذا الحقل مطلوب",
+                  })}></Textarea>
+                <FormErrorMessage>{errors.description?.message}</FormErrorMessage>
+              </FormControl>
+              <Stack mb={2} alignItems={"flex-start"}>
+                <Button
+                  w='fit-content'
+                  size='lg'
+                  p={6}
+                  bg={"brand.secondary"}
+                  color={"white"}
+                  _hover={{
+                    opacity: 0.8,
+                  }}
+                  _focus={{ opacity: 0.9, color: "white", outline: "none" }}
+                  _focusWithin={{
+                    opacity: 0.9,
+                    bg: "brand.secondary",
+                  }}
+                  _disabled={{
+                    bg: "#81EAFB6B",
+                    pointerEvents: "none",
+                  }}
+                  borderRadius={10}
+                  variant='solid'
+                  isLoading={isSubmitting}
+                  type='submit'>
+                  تاكيد
+                </Button>
+              </Stack>
+            </form>
           </ModalBody>
-      </ModalContent>
+        </ModalContent>
       </Modal>
     </Box>
-  )
-}
+  );
+};
 
 ManagePartners.getLayout = (page: ReactElement) => <Layout>{page}</Layout>;
 
-export default ManagePartners ;
+export default ManagePartners;
