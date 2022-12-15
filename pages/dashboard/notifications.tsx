78c36828a55be0fdc2f9d8c1b9ac95ac71ca9ea3
addy <addayounes8@gmail.com>
2022-12-15 15:43:25 +0100

    added pagination to notifications & reformated them

--- pages/dashboard/notifications.tsx
+++ pages/dashboard/notifications.tsx
@@ -1,43 +1,31 @@
-import Layout from '@/layouts/Dashboard';
-import { getPagination, ITEMS_IN_PAGE } from '@/utils/paginationConfig';
-import { Box, Button, Heading, Stack, Table, TableContainer, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react';
-import { ReactElement, useEffect, useState } from 'react'
+import PaginationBar from "@/components/PaginationBar";
+import Layout from "@/layouts/Dashboard";
+import { getPagination, ITEMS_IN_PAGE } from "@/utils/paginationConfig";
+import { Box, Heading, Table, TableContainer, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
+import { ReactElement, useEffect, useState } from "react";
 
 const Notifications = () => {
-  const [notification, setNotifications] = useState<any[]>([])
-  const [currentPage, setCurrentPage] = useState(0)
-  const [count, setCount] = useState(0)
+  const [notification, setNotifications] = useState<any[]>([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
-    new Promise( async () => {
-      const {from, to} = getPagination(currentPage, ITEMS_IN_PAGE);
+    new Promise(async () => {
+      const { from, to } = getPagination(currentPage, ITEMS_IN_PAGE);
       await fetch(`/api/notifications/get-notifications?from=${from}&to=${to}`)
-      .then((res) => res.json())
-      .then(data => {
-        console.log(data)
-        setNotifications(data.data)
-        setCount(data.count)
-      })
-    })
-  }, [currentPage])
-
-  const isLastPage = () => {
-    return (ITEMS_IN_PAGE * (currentPage+1) + ITEMS_IN_PAGE) > count
-  }
-
-  const handleNext = () => {
-    setCurrentPage(currentPage+1)
-  }
-
-  const handleBack = () => {
-    setCurrentPage(currentPage-1)
-  }
+        .then((res) => res.json())
+        .then((data) => {
+          setNotifications(data.data);
+          setCount(data.count);
+        });
+    });
+  }, [currentPage]);
 
   return (
     <Box px={8}>
-    <Heading>اشعارات</Heading>
-    <TableContainer bg="white" mt={16}>
-        <Table variant="simple">
+      <Heading>اشعارات</Heading>
+      <TableContainer bg='white' mt={16}>
+        <Table variant='simple'>
           <Thead>
             <Tr>
               <Th>ID</Th>
@@ -48,9 +36,15 @@ const Notifications = () => {
           </Thead>
           <Tbody>
             {notification?.map((d, i) => (
-              <Tr bg={d.color} key={d.id}>
-                 <Td>{d.id}</Td>
-                <Td>{d.text}</Td>
+              <Tr key={d.id}>
+                <Td>{d.id}</Td>
+                <Td position='relative' textColor={d.color !== "white" && d.color}>
+                  <span
+                    style={{ backgroundColor: d.color !== "white" ? d.color : "black" }}
+                    className='notification__types-badge'
+                  />
+                  {d.text}
+                </Td>
                 <Td>{new Date(d.created_at).toLocaleTimeString()}</Td>
                 <Td>{new Date(d.created_at).toLocaleDateString()}</Td>
               </Tr>
@@ -58,14 +52,16 @@ const Notifications = () => {
           </Tbody>
         </Table>
       </TableContainer>
-      <Stack direction="row">
-        {!isLastPage() && <Button onClick={handleNext}>التالي</Button>}
-        {currentPage !== 0 && <Button onClick={handleBack}>السابق</Button>}
-      </Stack>
+      <PaginationBar
+        max={count}
+        itemsPerPage={ITEMS_IN_PAGE}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
     </Box>
-  )
-}
+  );
+};
 
-Notifications .getLayout = (page: ReactElement) => <Layout>{page}</Layout>;
+Notifications.getLayout = (page: ReactElement) => <Layout>{page}</Layout>;
 
-export default Notifications
+export default Notifications;
