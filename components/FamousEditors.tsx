9c1e3eb5b94681312ea508bdf80a7c5f7a615308
addy <addayounes8@gmail.com>
2022-12-16 15:15:39 +0100

    added more padding to famous editors

--- components/FamousEditors.tsx
+++ components/FamousEditors.tsx
@@ -3,8 +3,6 @@ import {
   Box,
   Container,
   Flex,
-  Grid,
-  GridItem,
   SimpleGrid,
   VStack,
   chakra,
@@ -12,8 +10,8 @@ import {
 } from "@chakra-ui/react";
 
 import { FC } from "react";
-import { FamousAuthor } from "@/types/blog";
 import Link from "next/link";
+import { FamousAuthor } from "@/types/blog";
 
 type Props = {
   authors: FamousAuthor[];
@@ -23,30 +21,22 @@ const FamousEditor: FC<Props> = ({ authors }) => {
   return (
     <Box my={6} bg={useColorModeValue("brand.primary", "grey.900")}>
       <Flex
-        py={{ base: 6, md: 0 }}
-        minH="2xs"
+        py={8}
+        minH='2xs'
         as={Container}
-        maxW="container.xl"
+        maxW='container.xl'
         justify={{ base: "center", md: "space-between" }}
         align={{ base: "center", md: "center" }}
-        flexDirection={{ base: "column", md: "row" }}
-      >
+        flexDirection={{ base: "column", md: "row" }}>
         <chakra.span
-          color="white"
+          color='white'
           fontSize={{ base: "2xl", md: "3xl" }}
           fontWeight={600}
           lineHeight={{ base: "51.52px", md: "55.2px" }}
-          me={{ base: 0, md: 16 }}
-        >
+          me={{ base: 0, md: 16 }}>
           أشهر المحررين
         </chakra.span>
-        <SimpleGrid
-          w="full"
-          mt={{ base: 6, md: 0 }}
-          flex={1}
-          gap={6}
-          columns={{ base: 2, md: 6 }}
-        >
+        <SimpleGrid w='full' mt={{ base: 6, md: 0 }} flex={1} gap={6} columns={{ base: 2, md: 6 }}>
           {authors &&
             authors.map((author, index) => (
               <Box key={index}>
@@ -57,21 +47,19 @@ const FamousEditor: FC<Props> = ({ authors }) => {
                     src={`${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/avatars/${author.avatar_url}`}
                   />
                   <Link href={`/authors/${author.username}`} passHref>
-                    <Box _hover={{ cursor: "pointer" }} textAlign="center">
+                    <Box _hover={{ cursor: "pointer" }} textAlign='center'>
                       <chakra.span
                         fontWeight={600}
                         fontSize={{ base: "md", md: "xl" }}
-                        display="block"
-                        color="white"
-                      >
+                        display='block'
+                        color='white'>
                         {`${author.first_name} ${author.last_name}`}
                       </chakra.span>
                       <chakra.span
                         fontWeight={400}
                         fontSize={{ base: "sm", md: "md" }}
-                        display="block"
-                        color="white"
-                      >
+                        display='block'
+                        color='white'>
                         {author.speciality}
                       </chakra.span>
                     </Box>
