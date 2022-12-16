a789e7d720ce60bb944a87486931c208042aaf39
addy <addayounes8@gmail.com>
2022-12-16 22:15:44 +0100

    fixed team boxes width

--- pages/team.tsx
+++ pages/team.tsx
@@ -1,48 +1,62 @@
-import { Box, Button, ButtonGroup, Container, Divider, Flex, Heading, Stack, Text, Image, Avatar, useColorModeValue } from "@chakra-ui/react";
-import { CardHeader, CardBody, CardFooter, Card } from '@chakra-ui/card'
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Container,
+  Divider,
+  Flex,
+  Heading,
+  Stack,
+  Text,
+  Image,
+  Avatar,
+  useColorModeValue,
+  SimpleGrid,
+} from "@chakra-ui/react";
+import { CardHeader, CardBody, CardFooter, Card } from "@chakra-ui/card";
 
 import Layout from "@/layouts/Default";
 import { ReactElement, useEffect, useState } from "react";
-import Head from 'next/head';
+import Head from "next/head";
 
 function Team() {
-  const purpleTitles = useColorModeValue("brand.primary","brand.secondary")
-  const [data, setData] = useState<any[]>([])
+  const purpleTitles = useColorModeValue("brand.primary", "brand.secondary");
+  const [data, setData] = useState<any[]>([]);
 
   useEffect(() => {
     new Promise(async () => {
-      await fetch('/api/team')
-      .then((data) => data.json())
-      .then((data) => {
-
-        setData(data)
-      })
-    })
-  }, [setData])
+      await fetch("/api/team")
+        .then((data) => data.json())
+        .then((data) => {
+          setData(data);
+        });
+    });
+  }, [setData]);
   return (
     <>
-    <Head>
-      <title>تليد - فريقنا</title>
-    </Head>
-    <Container my="50px" maxW="container.xl">
-      <Box>
-        <Heading color={purpleTitles} fontSize="3xl">
-          فريق الادارة
-        </Heading>
+      <Head>
+        <title>تليد - فريقنا</title>
+      </Head>
+      <Container my='50px' maxW='container.xl'>
+        <Box>
+          <Heading color={purpleTitles} fontSize='3xl' mb={6}>
+            فريق الادارة
+          </Heading>
 
-        <Flex mt={"2em"} justify={"center"} wrap={"wrap"}>
-          {
-            data?.map(d => (
-              <Card key={d.id} maxW='14em' m="4">
+          <SimpleGrid w='full' gap={{ base: 2, md: 6 }} gridRow={1} minChildWidth='150px'>
+            {data?.map((d) => (
+              <Card key={d.id}>
                 <CardBody>
                   <Stack alignItems={"center"}>
                     <Avatar
-                      size={"2xl"}
+                      size={{ base: "xl", md: "2xl" }}
                       src={`${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/avatars/${d.avatar_url}`}
                       borderRadius='50%'
                     />
                     <Stack mt='6' spacing='3' alignItems={"center"}>
-                      <Heading fontSize={"0.8em"}>{d.first_name} {d.last_name}</Heading>
+                      <Heading textAlign='center' mt={2} fontSize={"0.8em"}>
+                        {d.first_name} {d.last_name}
+                      </Heading>
                       <Text textAlign={"center"} fontSize={"0.8em"}>
                         عضو في فريق {d.type}
                       </Text>
@@ -50,11 +64,10 @@ function Team() {
                   </Stack>
                 </CardBody>
               </Card>
-            ))
-          }
-        </Flex>
-      </Box>
-    </Container>
+            ))}
+          </SimpleGrid>
+        </Box>
+      </Container>
     </>
   );
 }
