98c0080191b6bd5f3eb88d5ebc3a3eb628dea285
addy <addayounes8@gmail.com>
2022-12-21 16:24:36 +0100

    fixed footer alignment

--- components/footer/SocialMediaLinks.tsx
+++ components/footer/SocialMediaLinks.tsx
@@ -1,9 +1,4 @@
-import {
-  Button,
-  ButtonGroupProps,
-  HStack,
-  useColorModeValue,
-} from "@chakra-ui/react";
+import { Box, Button, ButtonGroupProps, HStack, useColorModeValue } from "@chakra-ui/react";
 import {
   FaFacebookF,
   FaInstagram,
@@ -16,128 +11,121 @@ import {
 
 const SocialMediaLinks = (props: ButtonGroupProps) => {
   return (
-    <HStack spacing={8} flexWrap={"wrap"} justify="center" rowGap={8}>
+    <Box display='flex' flexWrap='wrap' justifyContent='center' alignItems='center' gap={8}>
       <Button
-        variant="unstyled"
-        as="a"
-        href="https://www.linkedin.com/company/taleed-foundation/"
-        target={"_blank"}
-        aria-label="Linkedin"
-        bgColor="white"
-        rounded="full"
+        variant='unstyled'
+        as='a'
+        href='https://www.linkedin.com/company/taleed-foundation/'
+        target='_blank'
+        aria-label='Linkedin'
+        bgColor='white'
+        rounded='full'
         color={useColorModeValue("brand.primary", "grey.900")}
         w={10}
         h={10}
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        flexWrap={"wrap"}
-      >
+        display='flex'
+        alignItems='center'
+        justifyContent='center'
+        flexWrap='wrap'>
         <FaLinkedin size={20} />
       </Button>
       <Button
-        variant="unstyled"
-        as="a"
-        href="https://twitter.com/Taleed01?t=4vsHk2jCyx4DRhVMyJSi4A&s=07"
-        target={"_blank"}
-        aria-label="Twitter"
-        bgColor="white"
-        rounded="full"
+        variant='unstyled'
+        as='a'
+        href='https://twitter.com/Taleed01?t=4vsHk2jCyx4DRhVMyJSi4A&s=07'
+        target='_blank'
+        aria-label='Twitter'
+        bgColor='white'
+        rounded='full'
         color={useColorModeValue("brand.primary", "grey.900")}
         w={10}
         h={10}
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-      >
+        display='flex'
+        alignItems='center'
+        justifyContent='center'>
         <FaTwitter size={20} />
       </Button>
       <Button
-        variant="unstyled"
-        as="a"
-        href="https://youtube.com/channel/UCAfKWA8D1tlUVBPUmPIrSFQ"
-        target={"_blank"}
-        aria-label="YouTube"
-        bgColor="white"
-        rounded="full"
+        variant='unstyled'
+        as='a'
+        href='https://youtube.com/channel/UCAfKWA8D1tlUVBPUmPIrSFQ'
+        target='_blank'
+        aria-label='YouTube'
+        bgColor='white'
+        rounded='full'
         color={useColorModeValue("brand.primary", "grey.900")}
         w={10}
         h={10}
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-      >
+        display='flex'
+        alignItems='center'
+        justifyContent='center'>
         <FaYoutube size={20} />
       </Button>
       <Button
-        variant="unstyled"
-        as="a"
-        href="https://instagram.com/taleed01?igshid=YmMyMTA2M2Y="
-        target={"_blank"}
-        aria-label="Instagram"
-        bgColor="white"
-        rounded="full"
+        variant='unstyled'
+        as='a'
+        href='https://instagram.com/taleed01?igshid=YmMyMTA2M2Y='
+        target='_blank'
+        aria-label='Instagram'
+        bgColor='white'
+        rounded='full'
         color={useColorModeValue("brand.primary", "grey.900")}
         w={10}
         h={10}
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-      >
+        display='flex'
+        alignItems='center'
+        justifyContent='center'>
         <FaInstagram size={20} />
       </Button>
       <Button
-        variant="unstyled"
-        as="a"
-        href="https://www.facebook.com/taleed01"
-        target={"_blank"}
-        aria-label="Facebook"
-        bgColor="white"
-        rounded="full"
+        variant='unstyled'
+        as='a'
+        href='https://www.facebook.com/taleed01'
+        target='_blank'
+        aria-label='Facebook'
+        bgColor='white'
+        rounded='full'
         color={useColorModeValue("brand.primary", "grey.900")}
         w={10}
         h={10}
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-      >
+        display='flex'
+        alignItems='center'
+        justifyContent='center'>
         <FaFacebookF size={20} />
       </Button>
       <Button
-        variant="unstyled"
-        as="a"
-        href="https://t.me/taleed01"
-        target={"_blank"}
-        aria-label="Telegram"
-        bgColor="white"
-        rounded="full"
+        variant='unstyled'
+        as='a'
+        href='https://t.me/taleed01'
+        target='_blank'
+        aria-label='Telegram'
+        bgColor='white'
+        rounded='full'
         color={useColorModeValue("brand.primary", "grey.900")}
         w={10}
         h={10}
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-      >
+        display='flex'
+        alignItems='center'
+        justifyContent='center'>
         <FaTelegramPlane size={20} />
       </Button>
       <Button
-        variant="unstyled"
-        as="a"
-        href="https://www.tiktok.com/@taleedplus"
-        target={"_blank"}
-        aria-label="TikTok"
-        bgColor="white"
-        rounded="full"
+        variant='unstyled'
+        as='a'
+        href='https://www.tiktok.com/@taleedplus'
+        target='_blank'
+        aria-label='TikTok'
+        bgColor='white'
+        rounded='full'
         color={useColorModeValue("brand.primary", "grey.900")}
         w={10}
         h={10}
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-      >
+        display='flex'
+        alignItems='center'
+        justifyContent='center'>
         <FaTiktok size={20} />
       </Button>
-    </HStack>
+    </Box>
   );
 };
 
--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -22,176 +22,158 @@ const Footer = () => {
   const jobVacancy = useDisclosure();
   return (
     <Box
-      as="footer"
-      role="contentinfo"
-      width={"100vw"}
-      bg={useColorModeValue("brand.primary", "grey.900")}
-    >
+      as='footer'
+      role='contentinfo'
+      width='100vw'
+      bg={useColorModeValue("brand.primary", "grey.900")}>
       <Contact isOpen={contact.isOpen} onClose={contact.onClose} />
       <Announce isOpen={announce.isOpen} onClose={announce.onClose} />
       <JobVacancy isOpen={jobVacancy.isOpen} onClose={jobVacancy.onClose} />
 
       <Flex
-        mx="auto"
+        mx='auto'
         maxW={{ md: "850px", lg: "900px", xl: "1000px" }}
         direction={{ base: "column", md: "row" }}
-        justify="space-between"
+        justify='space-between'
         align={{ base: "center", md: "flex-start" }}
         flexDirection={{ base: "column-reverse", lg: "row" }}
-        py={20}
-      >
+        py={20}>
         <Stack
           direction={{ base: "column-reverse", md: "column", lg: "row" }}
-          spacing={{ base: "12", md: "8" }}
-        >
-          <Stack direction="row" spacing={{ base: "10", md: "20" }}>
-            <Stack spacing="5">
-              <Text
-                fontSize={{ base: "xl", md: "2xl" }}
-                fontWeight="semibold"
-                color="white"
-              >
+          spacing={{ base: "12", md: "8" }}>
+          <Stack direction='row' spacing={{ base: "10", md: "20" }}>
+            <Stack spacing='5'>
+              <Text fontSize={{ base: "xl", md: "2xl" }} fontWeight='semibold' color='white'>
                 تواصل معنا
               </Text>
-              <Stack spacing="3" shouldWrapChildren>
+              <Stack spacing='3' shouldWrapChildren>
                 <Button
-                  as="a"
-                  bgColor="transparent"
-                  rounded="0px"
-                  cursor="pointer"
-                  borderBottom="2px solid"
-                  borderColor="transparent"
+                  as='a'
+                  bgColor='transparent'
+                  rounded='0px'
+                  cursor='pointer'
+                  borderBottom='2px solid'
+                  borderColor='transparent'
                   _active={{ bgColor: "transparent", borderColor: "white" }}
                   _hover={{ bgColor: "transparent", borderColor: "white" }}
                   aria-label={"تواصل معنا"}
-                  px="0"
-                  textAlign="center"
-                  fontWeight="500"
-                  color="white"
-                  onClick={contact.onOpen}
-                >
+                  px='0'
+                  textAlign='center'
+                  fontWeight='500'
+                  color='white'
+                  onClick={contact.onOpen}>
                   <Text fontSize={"lg"}>تواصل معنا</Text>
                 </Button>
                 <Button
-                  as="a"
-                  bgColor="transparent"
-                  rounded="0px"
-                  cursor="pointer"
-                  borderBottom="2px solid"
-                  borderColor="transparent"
+                  as='a'
+                  bgColor='transparent'
+                  rounded='0px'
+                  cursor='pointer'
+                  borderBottom='2px solid'
+                  borderColor='transparent'
                   _active={{ bgColor: "transparent", borderColor: "white" }}
                   _hover={{ bgColor: "transparent", borderColor: "white" }}
                   aria-label={"اعلن معنا"}
-                  px="0"
-                  textAlign="center"
-                  fontWeight="500"
-                  color="white"
-                  onClick={announce.onOpen}
-                >
+                  px='0'
+                  textAlign='center'
+                  fontWeight='500'
+                  color='white'
+                  onClick={announce.onOpen}>
                   <Text fontSize={"lg"}>اعلن معنا</Text>
                 </Button>
                 <Button
-                  as="a"
-                  bgColor="transparent"
-                  rounded="0px"
-                  cursor="pointer"
-                  borderBottom="2px solid"
-                  borderColor="transparent"
+                  as='a'
+                  bgColor='transparent'
+                  rounded='0px'
+                  cursor='pointer'
+                  borderBottom='2px solid'
+                  borderColor='transparent'
                   _active={{ bgColor: "transparent", borderColor: "white" }}
                   _hover={{ bgColor: "transparent", borderColor: "white" }}
                   aria-label={"مناصب شاغرة"}
-                  px="0"
-                  textAlign="center"
-                  fontWeight="500"
-                  color="white"
-                  onClick={jobVacancy.onOpen}
-                >
+                  px='0'
+                  textAlign='center'
+                  fontWeight='500'
+                  color='white'
+                  onClick={jobVacancy.onOpen}>
                   <Text fontSize={"lg"}>مناصب شاغرة</Text>
                 </Button>
               </Stack>
             </Stack>
-            <Stack spacing="5">
-              <Text
-                fontSize={{ base: "xl", md: "2xl" }}
-                fontWeight="semibold"
-                color="white"
-              >
+            <Stack spacing='5'>
+              <Text fontSize={{ base: "xl", md: "2xl" }} fontWeight='semibold' color='white'>
                 عن تليــد
               </Text>
-              <Stack spacing="3" shouldWrapChildren>
+              <Stack spacing='3' shouldWrapChildren>
                 <NextLink href={"/about"} passHref>
                   <Button
-                    as="a"
-                    bgColor="transparent"
-                    rounded="0px"
-                    cursor="pointer"
-                    borderBottom="2px solid"
-                    borderColor="transparent"
+                    as='a'
+                    bgColor='transparent'
+                    rounded='0px'
+                    cursor='pointer'
+                    borderBottom='2px solid'
+                    borderColor='transparent'
                     _active={{ bgColor: "transparent", borderColor: "white" }}
                     _hover={{ bgColor: "transparent", borderColor: "white" }}
                     aria-label={"من نحن"}
-                    px="0"
-                    textAlign="center"
-                    fontWeight="500"
-                    color="white"
-                  >
+                    px='0'
+                    textAlign='center'
+                    fontWeight='500'
+                    color='white'>
                     <Text fontSize={"lg"}>من نحن</Text>
                   </Button>
                 </NextLink>
                 <NextLink href={"/privacyPolicy"} passHref>
                   <Button
-                    as="a"
-                    bgColor="transparent"
-                    rounded="0px"
-                    cursor="pointer"
-                    borderBottom="2px solid"
-                    borderColor="transparent"
+                    as='a'
+                    bgColor='transparent'
+                    rounded='0px'
+                    cursor='pointer'
+                    borderBottom='2px solid'
+                    borderColor='transparent'
                     _active={{ bgColor: "transparent", borderColor: "white" }}
                     _hover={{ bgColor: "transparent", borderColor: "white" }}
                     aria-label={"سياسة الخصوصية"}
-                    px="0"
-                    textAlign="center"
-                    fontWeight="500"
-                    color="white"
-                  >
+                    px='0'
+                    textAlign='center'
+                    fontWeight='500'
+                    color='white'>
                     <Text fontSize={"lg"}>سياسة الخصوصية</Text>
                   </Button>
                 </NextLink>
                 <NextLink href={"/team"} passHref>
                   <Button
-                    as="a"
-                    bgColor="transparent"
-                    rounded="0px"
-                    cursor="pointer"
-                    borderBottom="2px solid"
-                    borderColor="transparent"
+                    as='a'
+                    bgColor='transparent'
+                    rounded='0px'
+                    cursor='pointer'
+                    borderBottom='2px solid'
+                    borderColor='transparent'
                     _active={{ bgColor: "transparent", borderColor: "white" }}
                     _hover={{ bgColor: "transparent", borderColor: "white" }}
                     aria-label={"فريق الادارة"}
-                    px="0"
-                    textAlign="center"
-                    fontWeight="500"
-                    color="white"
-                  >
+                    px='0'
+                    textAlign='center'
+                    fontWeight='500'
+                    color='white'>
                     <Text fontSize={"lg"}>فريق الادارة</Text>
                   </Button>
                 </NextLink>
                 <NextLink href={"/partners"} passHref>
                   <Button
-                    as="a"
-                    bgColor="transparent"
-                    rounded="0px"
-                    cursor="pointer"
-                    borderBottom="2px solid"
-                    borderColor="transparent"
+                    as='a'
+                    bgColor='transparent'
+                    rounded='0px'
+                    cursor='pointer'
+                    borderBottom='2px solid'
+                    borderColor='transparent'
                     _active={{ bgColor: "transparent", borderColor: "white" }}
                     _hover={{ bgColor: "transparent", borderColor: "white" }}
                     aria-label={"شركاؤنا"}
-                    px="0"
-                    textAlign="center"
-                    fontWeight="500"
-                    color="white"
-                  >
+                    px='0'
+                    textAlign='center'
+                    fontWeight='500'
+                    color='white'>
                     <Text fontSize={"lg"}>شركاؤنا</Text>
                   </Button>
                 </NextLink>
@@ -200,22 +182,24 @@ const Footer = () => {
           </Stack>
         </Stack>
 
-        <Stack
-          spacing={{ base: "10", lg: "45" }}
-          align="center"
-          mb={{ base: "60px", lg: "0" }}
-        >
+        <Box
+          display='flex'
+          flexDirection='column'
+          alignItems='center'
+          justifyContent='center'
+          gap={12}
+          mb={{ base: "60px", lg: 0 }}>
           <SocialMediaLinks />
           <Image
-            src="/logo.svg"
-            alt="talleed_logo"
+            src='/logo.svg'
+            alt='talleed_logo'
             w={{ base: "80px", md: "110px" }}
             h={{ base: "80px", md: "110px" }}
           />
-          <chakra.span fontWeight={"500"} color="white">
-            جميع الحقوق محفوظة &copy; 2022 موقع تليد
+          <chakra.span fontWeight='500' color='white'>
+            جميع الحقوق محفوظة &copy; {new Date().getFullYear()} موقع تليد
           </chakra.span>
-        </Stack>
+        </Box>
       </Flex>
     </Box>
   );
