f5d01252913d4c4807f745ae173f2529f22edcbb
addy <addayounes8@gmail.com>
2023-01-02 17:22:32 +0100

    editor: deleted premuim plugins

--- components/TinyEditor.tsx
+++ components/TinyEditor.tsx
@@ -3,64 +3,64 @@ import { Editor } from "@tinymce/tinymce-react";
 import { MutableRefObject } from "react";
 
 interface TinyEditorProps {
-  blogBody: MutableRefObject<any>;
-  initialValue?: string;
+    blogBody: MutableRefObject<any>;
+    initialValue?: string;
 }
 
 const TINY_EDITOR_API_KEY = "dz941oazvl971gmulquka7xp4dvh4tk130bombn0q3156kus";
 
 const tinyToolbar =
-  "undo redo | fontsize sizeselect | lineheight | bold italic backcolor forecolor | " +
-  "alignleft aligncenter alignright alignjustify | " +
-  "bullist numlist checklist outdent indent | removeformat | a11ycheck link image media code table help";
+    "undo redo | fontsize sizeselect | lineheight | bold italic backcolor forecolor | " +
+    "alignleft aligncenter alignright alignjustify | " +
+    "bullist numlist checklist outdent indent | removeformat | a11ycheck link image media code table help";
 
 const tinyPlugins = [
-  "a11ychecker",
-  "advlist",
-  "advcode",
-  "advtable",
-  "autolink",
-  "checklist",
-  "export",
-  "lists",
-  "link",
-  "image",
-  "charmap",
-  "preview",
-  "anchor",
-  "searchreplace",
-  "visualblocks",
-  "powerpaste",
-  "fullscreen",
-  "formatpainter",
-  "insertdatetime",
-  "media",
-  "table",
-  "help",
-  "wordcount",
+    // "a11ychecker",
+    "advlist",
+    // "advcode",
+    // "advtable",
+    "autolink",
+    // "checklist",
+    // "export",
+    "lists",
+    "link",
+    "image",
+    "charmap",
+    "preview",
+    "anchor",
+    "searchreplace",
+    "visualblocks",
+    // "powerpaste",
+    "fullscreen",
+    // "formatpainter",
+    "insertdatetime",
+    "media",
+    "table",
+    "help",
+    "wordcount",
 ];
 
 const TinyEditor: React.FC<TinyEditorProps> = ({ blogBody, initialValue = "<div></div>" }) => {
-  return (
-    <Editor
-      apiKey={TINY_EDITOR_API_KEY}
-      onInit={(_, editor) => (blogBody.current = editor)}
-      initialValue={initialValue}
-      init={{
-        skin: useColorModeValue("oxide", "oxide-dark"),
-        content_css: useColorModeValue("default", "dark"),
-        height: 500,
-        menubar: true,
-        plugins: tinyPlugins,
-        toolbar: tinyToolbar,
-        content_style:
-          "body { font-family:Comic Sans MS,Arial,sans-serif; font-size: 18pt; line-height: 1.6 }",
-        language: "ar",
-        line_height_formats: "1 1.2 1.4 1.6 1.8 2",
-        directionality: "rtl",
-      }}
-    />
-  );
+    return (
+        <Editor
+            apiKey={TINY_EDITOR_API_KEY}
+            onInit={(_, editor) => (blogBody.current = editor)}
+            initialValue={initialValue}
+            init={{
+                skin: useColorModeValue("oxide", "oxide-dark"),
+                content_css: useColorModeValue("default", "dark"),
+                height: 500,
+                menubar: true,
+                plugins: tinyPlugins,
+                toolbar: tinyToolbar,
+                content_style:
+                    "body { font-family:Comic Sans MS,Arial,sans-serif; font-size: 18pt; line-height: 1.6 }",
+                language: "ar",
+                line_height_formats: "1 1.2 1.4 1.6 1.8 2",
+                directionality: "rtl",
+            }}
+        />
+    );
 };
 
 export default TinyEditor;
