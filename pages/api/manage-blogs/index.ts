40bfcc7e7b1438b1810be8d32c21e2423fbcc5aa
addy <addayounes8@gmail.com>
2023-01-03 06:10:38 +0100

    fixed search by title & body

--- pages/api/manage-blogs/index.ts
+++ pages/api/manage-blogs/index.ts
@@ -1,21 +1,24 @@
 import { supabaseAdmin } from "@/utils/supabaseAdmin";
 import { NextApiHandler } from "next";
 
-const handler:NextApiHandler = async (req, res) => {
-  const { search } = req.query
+const handler: NextApiHandler = async (req, res) => {
+  const { search } = req.query;
 
-  if (search && req.method ===  "GET")  {
-    const {data, error } = await supabaseAdmin.from('posts')
-      .select("id,profiles(first_name,last_name,id), title, excerpt, thumbnail, category_id(id, name), tags")
-      .textSearch(
-        "title", search as string,
-        { type:"websearch"}
+  if (search && req.method === "GET") {
+    const { data, error } = await supabaseAdmin
+      .from("posts")
+      .select(
+        "id,profiles(first_name,last_name,id), created_at, title, excerpt, body, thumbnail, category_id(id, name), tags"
       )
-    if (error) {
-      res.status(500).json(error)
-    }
-    res.status(200).json({data})
-  }
-}
+      .eq("status", "published")
+      .or(`title.ilike.%${search}%,body.ilike.%${search}%`);
 
-export default handler
+    if (error) {
+      res.status(500).json(error);
+    }
+
+    res.status(200).json({ data });
+  }
+};
+
+export default handler;
